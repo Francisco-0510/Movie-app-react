e71ee02481432d01f43060c6f33a67c367af29ca
Francisco-0510 <francisco109623@gmail.com>
2024-10-28 13:39:32 -0600

    Revert "Metrics"
    This reverts commit f37e818c7c23e2e7d76909d1c27b5f8dd777fb9f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Layout from './components/Layout';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
 	return (
 		<Layout>
 			<Outlet />
-			<SpeedInsights />
 		</Layout>
 	);
 }
